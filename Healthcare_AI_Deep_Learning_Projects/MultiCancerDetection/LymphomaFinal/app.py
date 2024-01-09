17419ac3f59fb375caf23975ec506f6522842f4f
saisanthosh <santhosh.vc24@gmail.com>
2024-01-09 23:43:57 +0530

    Update app.py

--- Healthcare_AI_Deep_Learning_Projects/MultiCancerDetection/LymphomaFinal/app.py
+++ Healthcare_AI_Deep_Learning_Projects/MultiCancerDetection/LymphomaFinal/app.py
@@ -4,6 +4,17 @@ import streamlit as st
 from PIL import Image
 from tensorflow.keras.models import load_model
 import numpy as np
+import requests
+
+model_url = 'https://github.com/Sai-Santhosh/DataScienceProjects/blob/72f50431e957b325301393fc39b614d61bff50e0/Healthcare_AI_Deep_Learning_Projects/MultiCancerDetection/LymphomaFinal/Ensemble.h5'
+
+# Download the file
+r = requests.get(model_url)
+with open('Ensemble.h5', 'wb') as f:
+    f.write(r.content)
+
+# Now you can load your model
+model = load_model('Ensemble.h5')
 model = load_model("Ensemble.h5")
 def preprocess_image(image):
     image = image.resize((224, 224)) 
